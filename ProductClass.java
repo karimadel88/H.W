b1e951bdba9be16587b03c0a1db93edd458bd08a
Karim Adel <49067547+karimadel88@users.noreply.github.com>
2021-11-18 22:08:18 +0200

    Update ProductClass.java

--- ProductClass.java
+++ ProductClass.java
@@ -1,4 +1,5 @@
-public class Product {
+// package pacage.pro;
+class Product {
     private String name;
     private int numberOfPieces;
     public Product(String name) {
@@ -18,6 +19,18 @@ public class Product {
     public String toString() {
         return
                 "'" + name + '\'' + " " + numberOfPieces +
-                " Pieces ";
+                        " Pieces ";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product lamp = new Product("Lamp 60 Watt");
+        System.out.println("Before the loading: " + lamp);
+        lamp.downLoad(1000);
+        lamp.upLoad(100);
+        System.out.println("After the loading: " + lamp);
+
     }
 }
